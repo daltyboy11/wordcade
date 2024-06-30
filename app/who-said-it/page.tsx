409009dfbcef75d8adf1c413541917b6d86a640d
Dalton Sweeney <daltyboy11@gmail.com>
2024-06-29 22:18:22 -0400

    migrate who-said-it page to useGame hook

--- app/who-said-it/page.tsx
+++ app/who-said-it/page.tsx
@@ -1,59 +1,30 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
-import { useState, useEffect } from 'react';
 import { WhoSaidItQuestion } from '@/lib/who-said-it';
-import { dataFetcher } from '@/lib/data/data-fetcher';
+import { useGame } from '@/hooks/use-game';
+import { ClipLoader } from 'react-spinners';
+
+const validateWhoSaidItAnswer = (
+  question: WhoSaidItQuestion,
+  answerIndex: number
+) => {
+  return question.answer === answerIndex;
+};
 
 export default function WhoSaidIt() {
   const router = useRouter();
-  const [pageState, setPageState] = useState('pregame');
-  const [currentQuestion, setCurrentQuestion] = useState(0);
-  const [score, setScore] = useState(0);
-  const [timeLeft, setTimeLeft] = useState(30);
-  const [answers, setAnswers] = useState<number[]>([]);
-  const [data, setData] = useState<WhoSaidItQuestion[]>([]);
-
-  useEffect(() => {
-    const fetchGameData = async () => {
-      if (data.length === 0) {
-        const fetchedData = await dataFetcher.fetch('who-said-it');
-        setData(fetchedData);
-      }
-    };
-    fetchGameData();
-  }, [data, dataFetcher]);
-
-  useEffect(() => {
-    if (pageState === 'ingame' && timeLeft > 0) {
-      const timer = setTimeout(() => setTimeLeft(timeLeft - 1), 1000);
-      return () => clearTimeout(timer);
-    } else if (timeLeft === 0) {
-      setPageState('postgame');
-    }
-  }, [pageState, timeLeft]);
-
-  const handleStartGame = () => {
-    setPageState('ingame');
-    setCurrentQuestion(0);
-    setScore(0);
-    setTimeLeft(30);
-    setAnswers([]);
-  };
-
-  const handleAnswer = (index: number) => {
-    setAnswers([...answers, index]);
-    if (index === data[currentQuestion].answer) {
-      setScore(score + 1);
-    } else {
-      setScore(score - 1);
-    }
-    if (currentQuestion < data.length - 1) {
-      setCurrentQuestion(currentQuestion + 1);
-    } else {
-      setPageState('postgame');
-    }
-  };
+  const {
+    currentState,
+    previousState,
+    data,
+    startGame,
+    answerQuestion,
+    timeLeft,
+    score,
+    currentQuestion,
+    answers,
+  } = useGame('who-said-it', validateWhoSaidItAnswer);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-gradient-to-r from-purple-500 to-pink-500 text-white">
@@ -63,7 +34,8 @@ export default function WhoSaidIt() {
       >
         Back to Arcade
       </button>
-      {pageState === 'pregame' && (
+      {(currentState === 'pregame' ||
+        (currentState === 'loading-ingame' && previousState === 'pregame')) && (
         <div className="text-center">
           <h1 className="text-4xl font-bold mb-4">Who Said It?</h1>
           <p className="text-xl mb-8 max-w-xl mx-auto text-left">
@@ -71,16 +43,20 @@ export default function WhoSaidIt() {
             many as you can.
           </p>
           <button
-            onClick={handleStartGame}
+            onClick={startGame}
             style={{ minWidth: '200px' }}
             className="px-6 py-3 bg-purple-700 rounded-lg hover:bg-purple-800"
           >
-            Start
+            {currentState === 'loading-ingame' ? (
+              <ClipLoader color="#fff" size={16} />
+            ) : (
+              'Start'
+            )}
           </button>
         </div>
       )}
 
-      {pageState === 'ingame' && (
+      {currentState === 'ingame' && data && (
         <div className="text-center" style={{ minWidth: '300px' }}>
           <div className="mb-4">
             <h1 className="text-4xl font-bold" style={{ minWidth: '200px' }}>
@@ -94,7 +70,7 @@ export default function WhoSaidIt() {
             {data[currentQuestion].options.map((option, index) => (
               <button
                 key={index}
-                onClick={() => handleAnswer(index)}
+                onClick={() => answerQuestion(index)}
                 className="px-6 py-3 bg-purple-700 rounded-lg hover:bg-purple-800 w-64"
               >
                 {option}
@@ -104,40 +80,46 @@ export default function WhoSaidIt() {
         </div>
       )}
 
-      {pageState === 'postgame' && (
-        <div className="text-center">
-          <h1 className="text-4xl font-bold mb-4">Game Over</h1>
-          <p className="text-xl mb-8">Your score: {score}</p>
-          <h2 className="text-2xl mb-4 text-left">Your Answers:</h2>
-          <ul className="list-disc list-inside text-left">
-            {answers.map((answer, index) => {
-              const question = data[index];
-              const isCorrect = answer === question.answer;
-              return (
-                <li key={index} className="mb-2 italic">
-                  {question.quote}{' '}
-                  {isCorrect ? (
-                    <span className="text-green-500 not-italic">
-                      ✅ {question.options[answer]}
-                    </span>
-                  ) : (
-                    <span className="text-orange-300 not-italic">
-                      ❌ {question.options[answer]} (Correct:{' '}
-                      {question.options[question.answer]})
-                    </span>
-                  )}
-                </li>
-              );
-            })}
-          </ul>
-          <button
-            onClick={handleStartGame}
-            className="mt-8 px-6 py-3 bg-purple-700 rounded-lg hover:bg-purple-800"
-          >
-            Play Again
-          </button>
-        </div>
-      )}
+      {(currentState === 'postgame' ||
+        (currentState === 'loading-ingame' && previousState === 'postgame')) &&
+        data && (
+          <div className="text-center">
+            <h1 className="text-4xl font-bold mb-4">Game Over</h1>
+            <p className="text-xl mb-8">Your score: {score}</p>
+            <h2 className="text-2xl mb-4 text-left">Your Answers:</h2>
+            <ul className="list-disc list-inside text-left">
+              {answers.map(({ isCorrect, rawAnswer }, index) => {
+                const question = data[index];
+                const answer = question.options[rawAnswer];
+                const correctAnswer = question.options[question.answer];
+                return (
+                  <li key={index} className="mb-2 italic">
+                    {question.quote}{' '}
+                    {isCorrect ? (
+                      <span className="text-green-500 not-italic">
+                        ✅ {answer}
+                      </span>
+                    ) : (
+                      <span className="text-orange-300 not-italic">
+                        ❌ {answer} (Correct: {correctAnswer})
+                      </span>
+                    )}
+                  </li>
+                );
+              })}
+            </ul>
+            <button
+              onClick={startGame}
+              className="mt-8 px-6 py-3 bg-purple-700 rounded-lg hover:bg-purple-800"
+            >
+              {currentState === 'loading-ingame' ? (
+                <ClipLoader color="#fff" size={16} />
+              ) : (
+                'Play Again'
+              )}
+            </button>
+          </div>
+        )}
     </main>
   );
 }
